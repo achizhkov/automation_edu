441cc21217aa98a9f9f3af7cde3acfc23fdd6587
achizhkov <77821224+achizhkov@users.noreply.github.com>
2021-01-25 23:11:45 -0500

    Added a couple of functions

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,21 @@
 package ru.stqa.pft.sandbox;
 
+import java.sql.SQLOutput;
+
 public class MyFirstProgram {
-	public static void main (String[] args) {
-		System.out.println("Hello World");
+	public static void main(String[] args) {
+		hello("Viktor");
+		area(5.0,6.0);
 	}
-}
+
+	public static void hello(String name) {
+		System.out.println("Hello, " + name +" !");
+		}
+
+	public static double area(double a, double b) {
+		double sum = a * b;
+		System.out.println(sum);
+		return sum;
+	}
+
+	}
